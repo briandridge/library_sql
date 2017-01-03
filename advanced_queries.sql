048ea14b3024a6c68686e9a05574ca7de30684a3
briandridge <briandridge@gmail.com>
2017-01-03 16:45:46 -0700

    finished

--- advanced_queries.sql
+++ advanced_queries.sql
@@ -1,18 +1,107 @@
 -- ADVANCED / JOIN QUERIES
 -- For all of these queries, you SHOULD NOT use an id # in a WHERE clause
 
+
 -- Find all fields (book and author related) for all books written by George R.R. Martin.
+SELECT * FROM books JOIN authors ON (books.author_id = authors.id) WHERE name = 'George R.R. Martin';
+--  id |                title                | publication_date | author_id | id |        name        |       nationality        | birth_year 
+-- ----+-------------------------------------+------------------+-----------+----+--------------------+--------------------------+------------
+--  63 | A Game of Thrones                   |             1996 |         8 |  8 | George R.R. Martin | United States of America |       1945
+--  64 | A Clash of Kings                    |             1998 |         8 |  8 | George R.R. Martin | United States of America |       1945
+--  65 | A Storm of Swords                   |             2000 |         8 |  8 | George R.R. Martin | United States of America |       1945
+--  66 | A Feast for Crows                   |             2005 |         8 |  8 | George R.R. Martin | United States of America |       1945
+--  67 | A Dance with Dragons                |             2011 |         8 |  8 | George R.R. Martin | United States of America |       1945
+--  68 | I Swear the Dragons are Coming Soon |             2014 |         8 |  8 | George R.R. Martin | United States of America |       1945
+-- (6 rows)
 
 -- Find all fields (book and author related) for all books written by Milan Kundera.
+SELECT * FROM books JOIN authors ON (books.author_id = authors.id) WHERE name = 'Milan Kundera';
+--  id |                title                | publication_date | author_id | id |     name      |  nationality   | birth_year 
+-- ----+-------------------------------------+------------------+-----------+----+---------------+----------------+------------
+--  49 | The Joke                            |             1967 |         6 |  6 | Milan Kundera | Czechoslovakia |       1929
+--  50 | The Farewell Waltz                  |             1972 |         6 |  6 | Milan Kundera | Czechoslovakia |       1929
+--  51 | Life Is Elsewhere                   |             1973 |         6 |  6 | Milan Kundera | Czechoslovakia |       1929
+--  52 | The Book of Laughter and Forgetting |             1978 |         6 |  6 | Milan Kundera | Czechoslovakia |       1929
+--  53 | The Unbearable Lightness of Being   |             1984 |         6 |  6 | Milan Kundera | Czechoslovakia |       1929
+--  54 | Immortality                         |             1990 |         6 |  6 | Milan Kundera | Czechoslovakia |       1929
+--  55 | Slowness                            |             1995 |         6 |  6 | Milan Kundera | Czechoslovakia |       1929
+--  56 | Identity                            |             1998 |         6 |  6 | Milan Kundera | Czechoslovakia |       1929
+--  57 | Ignorance                           |             2000 |         6 |  6 | Milan Kundera | Czechoslovakia |       1929
+-- (9 rows)
 
 -- Find all books written by an author from China or the UK.
+SELECT * FROM books JOIN authors ON (books.author_id = authors.id) WHERE authors.nationality = 'China' OR authors.nationality = 'United Kingdom';
+--  id |                   title                   | publication_date | author_id | id |     name     |  nationality   | birth_year 
+-- ----+-------------------------------------------+------------------+-----------+----+--------------+----------------+------------
+--   1 | Dream of the Red Chamber                  |             1750 |         1 |  1 | Cao Xueqin   | China          |       1715
+--   2 | Harry Potter and the Philosophers Stone   |             1997 |         2 |  2 | J.K. Rowling | United Kingdom |       1965
+--   3 | Harry Potter and the Chamber of Secrets   |             1998 |         2 |  2 | J.K. Rowling | United Kingdom |       1965
+--   4 | Harry Potter and the Prisoner of Azkaban  |             1998 |         2 |  2 | J.K. Rowling | United Kingdom |       1965
+--   5 | Harry Potter and the Goblet of Fire       |             2000 |         2 |  2 | J.K. Rowling | United Kingdom |       1965
+--   6 | Harry Potter and the Order of the Phoenix |             2003 |         2 |  2 | J.K. Rowling | United Kingdom |       1965
+--   7 | Harry Potter and the Half-Blood Prince    |             2005 |         2 |  2 | J.K. Rowling | United Kingdom |       1965
+--   8 | Harry Potter and the Deathly Hallows      |             2007 |         2 |  2 | J.K. Rowling | United Kingdom |       1965
+--   9 | The Casual Vacancy                        |             2012 |         2 |  2 | J.K. Rowling | United Kingdom |       1965
+-- (9 rows)
 
 -- Find out how many books Albert Camus wrote.
+SELECT * FROM books JOIN authors ON (books.author_id = authors.id) WHERE name = 'Albert Camus';
+--  id |     title     | publication_date | author_id | id |     name     | nationality | birth_year 
+-- ----+---------------+------------------+-----------+----+--------------+-------------+------------
+--  58 | The Stranger  |             1942 |         7 |  7 | Albert Camus | France      |       1913
+--  59 | The Plague    |             1947 |         7 |  7 | Albert Camus | France      |       1913
+--  60 | The Fall      |             1956 |         7 |  7 | Albert Camus | France      |       1913
+--  61 | A Happy Death |             1971 |         7 |  7 | Albert Camus | France      |       1913
+--  62 | The First Man |             1995 |         7 |  7 | Albert Camus | France      |       1913
+-- (5 rows)
 
 -- Find out how many books written before 1980 were by authors not from the US.
+SELECT * FROM books JOIN authors ON (books.author_id = authors.id) WHERE publication_date < 1980 AND nationality != 'United States of America';
+--  id |                 title                 | publication_date | author_id | id |       name        |  nationality   | birth_year 
+-- ----+---------------------------------------+------------------+-----------+----+-------------------+----------------+------------
+--   1 | Dream of the Red Chamber              |             1750 |         1 |  1 | Cao Xueqin        | China          |       1715
+--  30 | Fervor de Buenos Aires                |             1923 |         4 |  4 | Jorge Luis Borges | Argentina      |       1899
+--  31 | Inquisiciones                         |             1925 |         4 |  4 | Jorge Luis Borges | Argentina      |       1899
+--  32 | Luna de Enfrente                      |             1925 |         4 |  4 | Jorge Luis Borges | Argentina      |       1899
+--  33 | El idioma de los argentinos           |             1928 |         4 |  4 | Jorge Luis Borges | Argentina      |       1899
+--  34 | Cuaderno San Martín                   |             1929 |         4 |  4 | Jorge Luis Borges | Argentina      |       1899
+--  35 | Discusión                             |             1957 |         4 |  4 | Jorge Luis Borges | Argentina      |       1899
+--  36 | Historia de la eternidad              |             1936 |         4 |  4 | Jorge Luis Borges | Argentina      |       1899
+--  37 | El jardín de senderos que se bifurcan |             1941 |         4 |  4 | Jorge Luis Borges | Argentina      |       1899
+--  38 | Seis problemas para don Isidro Parodi |             1942 |         4 |  4 | Jorge Luis Borges | Argentina      |       1899
+--  49 | The Joke                              |             1967 |         6 |  6 | Milan Kundera     | Czechoslovakia |       1929
+--  50 | The Farewell Waltz                    |             1972 |         6 |  6 | Milan Kundera     | Czechoslovakia |       1929
+--  51 | Life Is Elsewhere                     |             1973 |         6 |  6 | Milan Kundera     | Czechoslovakia |       1929
+--  52 | The Book of Laughter and Forgetting   |             1978 |         6 |  6 | Milan Kundera     | Czechoslovakia |       1929
+--  58 | The Stranger                          |             1942 |         7 |  7 | Albert Camus      | France         |       1913
+--  59 | The Plague                            |             1947 |         7 |  7 | Albert Camus      | France         |       1913
+--  60 | The Fall                              |             1956 |         7 |  7 | Albert Camus      | France         |       1913
+--  61 | A Happy Death                         |             1971 |         7 |  7 | Albert Camus      | France         |       1913
+-- (18 rows)
 
 -- For these last two, you should not need a JOIN.
 
 -- Find all authors whose names start with 'J'.
+SELECT * FROM authors WHERE name LIKE '%J%';
+--  id |       name        |       nationality        | birth_year 
+-- ----+-------------------+--------------------------+------------
+--   2 | J.K. Rowling      | United Kingdom           |       1965
+--   3 | James Baldwin     | United States of America |       1924
+--   4 | Jorge Luis Borges | Argentina                |       1899
+-- (3 rows)
 
 -- Find all books whose titles contain 'the'.
+SELECT * FROM books WHERE title LIKE '%the%' OR title LIKE 'The%';
+-- (35 rows)
+
+
+
+-- AUTHORS;
+--  id |        name        |       nationality        | birth_year 
+-- ----+--------------------+--------------------------+------------
+--   1 | Cao Xueqin         | China                    |       1715
+
+-- BOOKS;
+--  id |                      title                       | publication_date | author_id 
+-- ----+--------------------------------------------------+------------------+-----------
+--   1 | Dream of the Red Chamber                         |             1750 |         1
--- basic_queries.sql
+++ basic_queries.sql
@@ -2,30 +2,95 @@
 -- Unless otherwise stated, all queries should return all columns
 
 -- Get all information about all authors
-
+SELECT * FROM authors;
 -- Get just the name and birth year of all authors
+SELECT name, birth_year FROM authors;
+--         name        | birth_year 
+-- --------------------+------------
+--  Cao Xueqin         |       1715
+--  J.K. Rowling       |       1965
+--  James Baldwin      |       1924
+--  Jorge Luis Borges  |       1899
+--  Haruki Murakami    |       1949
+--  Milan Kundera      |       1929
+--  Albert Camus       |       1913
+--  George R.R. Martin |       1945
+-- (8 rows)
 
 -- Get all authors born in the 20th centruy or later
-
+SELECT * FROM authors WHERE birth_year >= 1900;
+--  id |        name        |       nationality        | birth_year 
+-- ----+--------------------+--------------------------+------------
+--   2 | J.K. Rowling       | United Kingdom           |       1965
+--   3 | James Baldwin      | United States of America |       1924
+--   5 | Haruki Murakami    | Japan                    |       1949
+--   6 | Milan Kundera      | Czechoslovakia           |       1929
+--   7 | Albert Camus       | France                   |       1913
+--   8 | George R.R. Martin | United States of America |       1945
+-- (6 rows)
 -- Get all authors born in the USA
+SELECT * FROM authors WHERE nationality = 'United States of America';
+--  id |        name        |       nationality        | birth_year 
+-- ----+--------------------+--------------------------+------------
+--   3 | James Baldwin      | United States of America |       1924
+--   8 | George R.R. Martin | United States of America |       1945
+-- (2 rows)
 
 -- Get all books published on 1985
+SELECT * FROM books WHERE publication_date = 1985;
+--  id |              title              | publication_date | author_id 
+-- ----+---------------------------------+------------------+-----------
+--  26 | The Evidence of Things Not Seen |             1985 |         3
+--  27 | The Price of the Ticket         |             1985 |         3
+-- (2 rows)
 
 -- Get all books published before 1989
-
+SELECT * FROM books WHERE publication_date < 1989;
+--  id |                      title                       | publication_date | author_id 
+-- ----+--------------------------------------------------+------------------+-----------
+--   1 | Dream of the Red Chamber                         |             1750 |         1
+-- .....................................................................................
+--  61 | A Happy Death                                    |             1971 |         7
+(38 rows)
 -- Get just the title of all books.
+SELECT title FROM books;
 
 -- Get just the year that 'A Dance with Dragons' was published
   -- Cry when you realize how long it's been
+  SELECT publication_date FROM books WHERE title = 'A Dance with Dragons';
+--    publication_date 
+-- ------------------
+--              2011
+-- (1 row)
 
 -- Get all books which have `the` somewhere in their title (hint, look up LIKE/ILIKE)
+SELECT * FROM books WHERE title LIKE '%the%';
+-- (20 rows)
 
 -- Add yourself as an author
+INSERT INTO authors VALUES (9, 'Brian Ridge', 'United States of America', 1986);
 
 -- Add two books that you'd like to write (you can hard-code your id as the author id)
+INSERT INTO books VALUES (69, 'How to be awesome like me', 2017, 9);
+INSERT INTO books VALUES (70, 'Get Rich or Die Coding', 2017, 9);
 
 -- Update one of your books to have a new title
+UPDATE books SET title = 'How to be really awesome like me' WHERE title = 'How to be awesome like me';
 
 -- Delete your books
+DELETE FROM books where author_id = 9;
 
 -- Delete your author entry
+DELETE FROM authors where id = 9;
+
+
+
+-- library=# SELECT * FROM AUTHORS;
+--  id |        name        |       nationality        | birth_year 
+-- ----+--------------------+--------------------------+------------
+--   1 | Cao Xueqin         | China                    |       1715
+
+-- library=# SELECT * FROM books;
+--  id |                      title                       | publication_date | author_id 
+-- ----+--------------------------------------------------+------------------+-----------
+--   1 | Dream of the Red Chamber                         |             1750 |         1
